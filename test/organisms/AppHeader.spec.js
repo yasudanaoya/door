e5c5bcd345f2607715a3988e579cf12782d58799
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2021-06-26 14:08:40 +0900

    fix: lint error

--- test/organisms/AppHeader.spec.js
+++ test/organisms/AppHeader.spec.js
@@ -1,17 +1,17 @@
-import { createLocalVue, shallowMount } from "@vue/test-utils"
-import VueCompositionApi from "@vue/composition-api"
-import Vuetify from "vuetify"
-import Component from "~/components/organisms/AppHeader"
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import VueCompositionApi from '@vue/composition-api'
+import Vuetify from 'vuetify'
+import Component from '~/components/organisms/AppHeader'
 
 const localVue = createLocalVue()
 localVue.use(VueCompositionApi)
 localVue.use(Vuetify)
 
-describe("AppHeader", () => {
-  test("snapshot", () => {
+describe('AppHeader', () => {
+  test('snapshot', () => {
     const wrapper = shallowMount(Component, {
       propsData: {},
-      localVue
+      localVue,
     })
     expect(wrapper.element).toMatchSnapshot()
   })
