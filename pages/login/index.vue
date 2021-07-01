32405406af7e411b88ec82ca971a04c2878cff3e
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2021-07-02 01:13:27 +0900

    feat: firebase login

---
+++ pages/login/index.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  div
+    v-btn(@click='login') Login
+    v-btn(@click='logout') Logout
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+import firebase from '~/plugins/firebase'
+
+export default defineComponent({
+  setup() {
+    const login = () => {
+      firebase.auth().signInWithRedirect(new firebase.auth.GithubAuthProvider())
+    }
+
+    const logout = () => {
+      firebase.auth().signOut()
+    }
+
+    return {
+      login,
+      logout,
+    }
+  },
+})
+</script>
