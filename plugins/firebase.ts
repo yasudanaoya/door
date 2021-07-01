55ed45e550a403caf8994d9c051a514421a4101f
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2021-07-02 01:15:30 +0900

    fix: config

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -1,14 +1,12 @@
 import firebase from 'firebase'
 
-// ** Firebaseプロジェクトの設定を記す
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: process.env.FIREBASE_API_KEY, // web api key
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: `${process.env.FIREBASE_ID}.firebaseapp.com`,
     databaseURL: `https://${process.env.FIREBASE_ID}.firebaseio.com`,
     projectId: `${process.env.FIREBASE_ID}`,
     storageBucket: `${process.env.FIREBASE_ID}.appspot.com`,
-    messagingSenderId: '1234567890', // ** cloudmessagingを使う場合は設定
   })
 }
 
